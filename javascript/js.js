ec182403ccbbddc6f8ca80cb2ac8c0e865757a15
usuario <usuario@email.com>
2022-12-25 11:45:10 -0600

    se añadieron estilos sobre el logotipo en formato svg

--- javascript/js.js
+++ javascript/js.js
@@ -2,5 +2,8 @@ $(document).ready(function () {
   $('.texto').focus(function () {
     $('.texto').addClass("textoDesplegado");
   })
+  $('.encriptacion').focus(function () {
+    $('.encriptacion').addClass("textoDesplegado");
+  })
 })
 
