f726fc7d49c8e65446ef427af4af680c841dccc8
Thebyren <byronrena1@gmail.com>
2022-12-31 18:06:49 -0600

    [new] se agregaron estilos a los botones, para desktop como responsivos, se agrego la propiedad user-select:none; al body para evitar la seleccion de texto decorativo como los botones o las imagenes

--- javascript/js.js
+++ javascript/js.js
@@ -1,24 +1,32 @@
 $(document).ready(function () {
+
+  //---------------------------[funcion reguladora de altura]---------------------------//
   $('.texto').on('input', function () {
     $(this).css('height', 'auto')
-    var height = Math.min(this.scrollHeight, 200)
-    $(this).css('height', height + 'px')
+    // calcula la altura del textarea y agrega espacio de altura segun la necesidad
+    var height = Math.min(this.scrollHeight, 150)
+    $(this).css('height', height + 'px') // se manipula la altura usando la variable height
   })
+  
+  
+  //-------------------------[funcion de monitereo de caracteres]-------------------------//
   $('.texto').on('keydown', function () {
     // Obtiene el valor del textarea y calcula la cantidad de caracteres
     var texto = $(this).val()
     var cantidad = texto.length
     // Actualiza el elemento con el ID "contador" con la cantidad de caracteres
     $('#contador').text(cantidad)
-
+    
+    //[agregado, cambia el estilo del contado segun la cantidad de caracteres insertados]//
+    function colorDeContador(color) {
+      document.querySelector('#contador').setAttribute('style', color);
+    } 
     if (cantidad > 250) {
-      document
-        .querySelector('#contador')
-        .setAttribute('style', 'color : rgb(204, 111, 111)')
+      colorDeContador('color : rgb(204, 111, 111)')
     } else if (cantidad > 200) {
-      document.querySelector('#contador').setAttribute('style', 'color: rgb(240, 201, 27)')
+      colorDeContador('color: rgb(240, 201, 27)')
     } else {
-      document.querySelector('#contador').setAttribute('style', 'color: #777')
+      colorDeContador('color: #777')
     }
   })
 })
